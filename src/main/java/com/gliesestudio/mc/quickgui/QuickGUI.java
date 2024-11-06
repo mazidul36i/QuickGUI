aa3f1b21848620293bd93ef9313230da1403a317
Mazidul Islam <mazidul.islam@prospecta.com>
2024-11-07 01:18:40 +0530

    Initial commit

---
+++ src/main/java/com/gliesestudio/mc/quickgui/QuickGUI.java
@@ -0,0 +1,17 @@
+package com.gliesestudio.mc.quickgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QuickGUI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
